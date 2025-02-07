63c709d6fd8b9400c5ff8cbfe76366d1d3e17f7d
Konstantin Rotaermel <202117220+konstrot@users.noreply.github.com>
2025-02-07 23:31:57 +0100

    Create class Node

---
+++ rbtree.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, key, color='RED'):
+        self.key = key
+        self.color = color
+        self.left = None
+        self.right = None
+        self.parent = None
+
+class RedBlackTree:
+    def __init__(self):
+        self
