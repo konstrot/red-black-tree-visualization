257005a603b1660dae24338b1c317ea9ff7ee793
Konstantin Rotaermel <202117220+konstrot@users.noreply.github.com>
2025-02-09 21:56:35 +0100

    Add insert

--- rbtree.py
+++ rbtree.py
@@ -10,3 +10,31 @@ class RedBlackTree:
     def __init__(self):
         self.NIL = Node(None, color='BLACK')  # Create NIL
         self.root = self.NIL  # Create root
+
+    def insert(self, key):  # Insert
+        new_node = Node(key)
+        new_node.left = self.NIL
+        new_node.right = self.NIL
+
+        parent = None
+        current = self.root
+
+        while current != self.NIL:
+            parent = current
+            if key < current.key:
+                current = current.left
+            else:
+                current = current.right
+
+        new_node.parent = parent
+        if parent is None:
+            self.root = new_node
+            self.root.color = "BLACK"
+        elif key < parent.key:
+            parent.left = new_node
+        else:
+            parent.right = new_node
+
+        self._fix_insert(new_node)
+
+        
